e39a13cd9be58b09701c2d09b163cc1799fcf55b
dev-fath <ggje33@gmail.com>
2022-05-17 15:10:18 +0900

    android sentry, codepush 설정 추가

--- android/app/src/main/java/com/rncarmeleon/MainApplication.java
+++ android/app/src/main/java/com/rncarmeleon/MainApplication.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import io.sentry.react.RNSentryPackage;
-import io.sentry.react.RNSentryPackage;
-import io.sentry.react.RNSentryPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
