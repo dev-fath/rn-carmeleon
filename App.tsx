72887495b6349f80c544883e8daea663d8bbcb83
dev-fath <ggje33@gmail.com>
2022-05-17 09:36:19 +0900

    code push 설정 추가

--- App.tsx
+++ App.tsx
@@ -8,9 +8,10 @@
  * @format
  */
 
-import React from 'react';
+import React, { ComponentType } from 'react';
 import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import * as Sentry from '@sentry/react-native';
+import codePush from 'react-native-code-push';
 
 Sentry.init({
   dsn: 'https://12655d0c152e4b9e9a849a99de3b3bb0@o1243005.ingest.sentry.io/6397948',
@@ -58,7 +59,21 @@ const Section: React.FC<{
   );
 };
 
-const App = () => {
+const codePushOptions: { [index: string]: unknown } = {
+  deploymentKey: '4c364078-1c74-4f24-9820-da19500e830d',
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+  // 언제 업데이트를 체크하고 반영할지를 정한다.
+  // ON_APP_RESUME은 Background에서 Foreground로 오는 것을 의미
+  // ON_APP_START은 앱이 실행되는(켜지는) 순간을 의미
+  updateDialog: false,
+  // 업데이트를 할지 안할지 여부에 대한 노출 (잠수함 패치의 경우 false)
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  installMode: codePush.InstallMode.IMMEDIATE,
+  // 업데이트를 어떻게 설치할 것인지 (IMMEDIATE는 강제설치를 의미)
+};
+
+const AppLayout = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -113,5 +128,6 @@ const styles = StyleSheet.create({
     fontWeight: '700',
   },
 });
-
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+const App = codePush(codePushOptions)(AppLayout) as ComponentType;
 export default Sentry.wrap(App);
