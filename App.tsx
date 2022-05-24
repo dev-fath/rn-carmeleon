05a2bee99004a904fe0e8a8bdd41aedde3595f8a
dev-fath <ggje33@gmail.com>
2022-05-24 09:42:37 +0900

    [메인] 메인화면 레이아웃 배치

--- App.tsx
+++ App.tsx
@@ -27,14 +27,12 @@ Sentry.init({
 
 const codePushOptions: { [index: string]: unknown } = {
   deploymentKey: '4c364078-1c74-4f24-9820-da19500e830d',
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
   // 언제 업데이트를 체크하고 반영할지를 정한다.
   // ON_APP_RESUME은 Background에서 Foreground로 오는 것을 의미
   // ON_APP_START은 앱이 실행되는(켜지는) 순간을 의미
   updateDialog: false,
   // 업데이트를 할지 안할지 여부에 대한 노출 (잠수함 패치의 경우 false)
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   installMode: codePush.InstallMode.IMMEDIATE,
   // 업데이트를 어떻게 설치할 것인지 (IMMEDIATE는 강제설치를 의미)
 };
--- src/page/Main.tsx
+++ src/page/Main.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
-import { ScrollView, Text } from 'react-native';
+import { Text, SafeAreaView, View, TouchableWithoutFeedback } from 'react-native';
 import MenuButton from '../components/MenuButton';
 
 const MainComponent = () => {
   return (
-    <ScrollView contentInsetAdjustmentBehavior="automatic">
-      <MenuButton />
-      <Text>Main page</Text>
-    </ScrollView>
+    <SafeAreaView>
+      <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+        <MenuButton />
+        <Text>로고</Text>
+        <TouchableWithoutFeedback>
+          <Text>자동결제등록</Text>
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback>
+          <Text>검색버튼</Text>
+        </TouchableWithoutFeedback>
+      </View>
+      <View style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Text>지도</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
