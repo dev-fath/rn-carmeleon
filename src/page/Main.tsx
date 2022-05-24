65cbbb82c8e4cd21fac7e9190f4ce661799d916e
dev-fath <ggje33@gmail.com>
2022-05-24 10:48:15 +0900

    [서비스메뉴] 서비스메뉴 칩 추가

--- src/page/Main.tsx
+++ src/page/Main.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { Text, View } from 'react-native';
 import TitleBar from '../components/TitleBar';
+import ServiceList from '../components/ServiceList';
 
 const MainComponent = () => {
   return (
     <View>
       <TitleBar />
+      <ServiceList />
       <View style={{ justifyContent: 'center', alignItems: 'center' }}>
         <Text>지도</Text>
       </View>
