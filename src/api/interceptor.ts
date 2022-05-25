eba37cbd5bea5b9b59d2ffef0a538b5c4bab4c51
dev-fath <ggje33@gmail.com>
2022-05-25 14:15:54 +0900

    [api] axios 객체 랩핑

--- src/api/interceptor.ts
+++ src/api/interceptor.ts
@@ -1,12 +1,14 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
 const axiosConfig: AxiosRequestConfig<unknown> = { baseURL: 'http://localhost:3000' };
-const client = axios.create(axiosConfig);
+const axiosClient = axios.create(axiosConfig);
 
-client.interceptors.response.use(res => {
+axiosClient.interceptors.response.use(res => {
   return res.data as unknown;
 });
 
-client.interceptors.request.use(req => {
+axiosClient.interceptors.request.use(req => {
   return req;
 });
+
+export default axiosClient;
