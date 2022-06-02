2deda14ae3c013aaeff3807188cc8aa9ff741430
dev-fath <ggje33@gmail.com>
2022-06-02 12:16:50 +0900

    [로그인] 로그인 페이지 배경 그라데이션 추가

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -1,31 +1,48 @@
 import React from 'react';
 
 import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 import BackButton from '../components/BackButton';
 
 const LoginPage = () => {
   return (
     <SafeAreaView>
-      <BackButton />
-      <View>
-        <Text>첫 로그인 접속하시면</Text>
-        <Text>100 포인트.</Text>
-      </View>
-      <View>
-        <Text>간편하게 시작해볼까요 :)</Text>
-        <TouchableOpacity>
-          <Text>네이버로 계속하기</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>카카오로 계속하기</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>Apple로 계속하기</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>Google로 계속하기</Text>
-        </TouchableOpacity>
-      </View>
+      <LinearGradient
+        style={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          alignContent: 'center',
+          justifyContent: 'space-between',
+        }}
+        colors={['#34AFEF', '#557FD7', '#557FD7']}
+        start={{ x: 3.25, y: 0.75 }}
+        end={{ x: 1, y: 2.25 }}
+      >
+        <BackButton />
+        <View>
+          <Text>첫 로그인 접속하시면</Text>
+          <Text>100 포인트.</Text>
+        </View>
+        <View>
+          <Text style={{ textAlign: 'center' }}>간편하게 시작해볼까요 :)</Text>
+          <TouchableOpacity style={{ alignItems: 'center' }}>
+            <Text>네이버로 계속하기</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ alignItems: 'center' }}>
+            <Text>카카오로 계속하기</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ alignItems: 'center' }}>
+            <Text>Apple로 계속하기</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ alignItems: 'center' }}>
+            <Text>Google로 계속하기</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Text>로고</Text>
+        </View>
+      </LinearGradient>
     </SafeAreaView>
   );
 };
