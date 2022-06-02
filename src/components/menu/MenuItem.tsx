f31928cf1da7ee8291c915b846cb323aebd643a4
dev-fath <ggje33@gmail.com>
2022-06-02 15:50:37 +0900

    [로그인] 각 메뉴별 접근권한 처리

--- src/components/menu/MenuItem.tsx
+++ src/components/menu/MenuItem.tsx
@@ -5,13 +5,30 @@ import { useNavigation } from '@react-navigation/native';
 import { MenuScreenNavigationProp } from '../../interfaces/navigation';
 import { MenuItemInterface } from '../../interfaces/menuItem';
 
-const MenuItem = ({ icon, name, title }: MenuItemInterface) => {
+const MenuItem = ({ icon, name, title, needAuth, modalVisible = false, setModalVisible }: MenuItemInterface) => {
   const menuNavigation: MenuScreenNavigationProp = useNavigation();
+
+  function navigateOnPress() {
+    if (needAuth) {
+      //TODO : 로그인 여부 불러오기
+      const isAuth = false;
+      if (isAuth) {
+        menuNavigation.navigate(name);
+      } else {
+        if (setModalVisible) {
+          setModalVisible(!modalVisible);
+        }
+      }
+      return;
+    }
+    menuNavigation.navigate(name);
+    console.warn(title);
+  }
+
   return (
     <TouchableWithoutFeedback
       onPress={() => {
-        menuNavigation.navigate(name);
-        console.warn(title);
+        navigateOnPress();
       }}
     >
       <View style={{ width: '50%', display: 'flex', flexDirection: 'row', justifyContent: 'center', padding: 16 }}>
--- src/page/menus/Menu.tsx
+++ src/page/menus/Menu.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
-import { SafeAreaView, Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Pressable, SafeAreaView, Text, TouchableWithoutFeedback, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { ColorTheme } from '../../../assets/colorCodes';
 import MenuList from '../../components/menu/MenuList';
+import { DefaultScreenNavigationProp, MenuScreenNavigationProp } from '../../interfaces/navigation';
 
 const Menu = () => {
-  const menuNavigation = useNavigation();
+  const menuNavigation: MenuScreenNavigationProp = useNavigation();
+  const mainNavigation: DefaultScreenNavigationProp = useNavigation();
+
   return (
     <>
       <SafeAreaView style={{ backgroundColor: ColorTheme.Main }}>
@@ -26,7 +29,13 @@ const Menu = () => {
         </View>
         <View style={{ margin: 24 }}>
           <Text style={{ color: ColorTheme.white }}>첫 로그인 접속하시면 100 포인트를 드려요.</Text>
-          <Text style={{ color: ColorTheme.white }}>회원가입/로그인하기&gt; </Text>
+          <Pressable
+            onPress={() => {
+              mainNavigation.navigate('Login');
+            }}
+          >
+            <Text style={{ color: ColorTheme.white }}>회원가입/로그인하기&gt; </Text>
+          </Pressable>
         </View>
       </SafeAreaView>
       <MenuList />
